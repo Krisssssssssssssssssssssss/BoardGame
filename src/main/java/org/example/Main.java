82e8e80833479ce77b4e9f8ff4ee584c79b7454e
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-02 12:45:49 +0200

    Some refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,20 +16,4 @@ class Main {
             renderer.render();
         }
     }
-
-    public static int[] moveLeft(int[] boardSize, int[]playerPosition) {
-        if (playerPosition[1] == 0) {
-            return playerPosition;
-        } else {
-            return new int[]{playerPosition[0], playerPosition[1] - 1};
-        }
-    }
-
-    public static int[] moveRight(int[] boardSize, int[]playerPosition) {
-        if (playerPosition[1] == boardSize[1] - 1) {
-            return playerPosition;
-        } else {
-            return new int[]{playerPosition[0], playerPosition[1] + 1};
-        }
-    }
 }
