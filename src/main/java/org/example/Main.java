e2348d012fe62fbff128700f89b383373bca6f5f
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-02 10:20:19 +0200

    [WIP] MoveUp test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import jdk.internal.net.http.common.Pair;
+
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean[][] boardBad = {
+                {true, false, false},
+                {false, false, false},
+                {false, false, false},
+        };
+
+        int[] boardSizeGood = {5, 5};
+
+    }
+
+    public static boolean[][] moveUpBad(boolean[][] board) {
+        // Check if player cannot move up.
+        for (boolean element : board[0]) {
+            if (element) return board;
+        }
+
+        // Find player current position.
+        int playerLine = 0;
+        int playerRow = 0;
+        for (int lineNumber = 1; lineNumber < board.length; lineNumber++) {
+            for (int columnNumber = 0; columnNumber < board[lineNumber].length; columnNumber++) {
+                if (board[lineNumber][columnNumber]) {
+                    playerLine = lineNumber;
+                    playerRow = columnNumber;
+                }
+            }
+        }
+
+        // Set player new position.
+        board[playerLine][playerRow] = false;
+        board[playerLine - 1][playerRow] = true;
+    }
+
+    public static int[] moveUpGood(int[] boardSize, int[]playerPosition) {
+        if (playerPosition[1] == 0) {
+            return playerPosition;
+        } else {
+            int[] result = {playerPosition[0], playerPosition[1] - 1};
+            return result;
+        }
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,10 +2,51 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+// Your task is to develop a simple 2D game board and a game character.
+// The game board is a grid on which you can move using the W (up), S (down), D (right), and A (left) keys.
+// You will write tests to ensure that the game character moves correctly on the game board.
 class MainTest {
 
     @org.junit.jupiter.api.Test
-    void main() {
+    void testInput() {
+
+    }
+
+    void testMoveUp_possible() {
+        boolean[][] board = {
+                {false, false, false},
+                {true, false, false},
+                {false, false, false},
+        };
+        boolean[][] expected = {
+                {true, false, false},
+                {false, false, false},
+                {false, false, false},
+        };
+
+        boolean[][] actual = Main.moveUpBad(board);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    void testMoveUp_impossible() {
+        boolean[][] board = {
+                {true, false, false},
+                {false, false, false},
+                {false, false, false},
+        };
+    }
+
+    void testMoveDown() {
+
+    }
+
+    void testMoveLeft() {
+
+    }
+
+    void testMoveRight() {
 
     }
 }
