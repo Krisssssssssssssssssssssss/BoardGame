8bcdaa6968b82762748a73162b46099ea1a82e8b
Kristijan Jakimov <kiko.jakimov@gmail.com>
2024-09-02 11:59:22 +0200

    Left to Right added

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -74,12 +74,62 @@ class MainTest {
         assertEquals(expectedPlayerX, actualPlayerX);
         assertEquals(expectedPlayerY, actualPlayerY);
     }
-
+    //Left
+    @Test
     void testMoveLeft() {
+        char input = 'A';
+        int[] boardSize = {8, 8};
+        PlayerCharacter playerCharacter = new PlayerCharacter(3, 0, boardSize);
+        int expectedPlayerX = 2;
+        int expectedPlayerY = 0;
+        playerCharacter.move(input);
+        int actualPlayerX = playerCharacter.getX();
+        int actualPlayerY = playerCharacter.getY();
 
+        assertEquals(expectedPlayerX, actualPlayerX);
+        assertEquals(expectedPlayerY, actualPlayerY);
     }
+    @Test
+    void testMoveLeftimpossible() {
+        char input = 'A';
+        int[] boardSize = {8, 8};
+        PlayerCharacter playerCharacter = new PlayerCharacter(0,0, boardSize);
+        int expectedPlayerX = 0;
+        int expectedPlayerY = 0;
+        playerCharacter.move(input);
+        int actualPlayerX = playerCharacter.getX();
+        int actualPlayerY = playerCharacter.getY();
 
+        assertEquals(expectedPlayerX, actualPlayerX);
+        assertEquals(expectedPlayerY, actualPlayerY);
+    }
+    //Right
+    @Test
     void testMoveRight() {
+        char input = 'D';
+        int[] boardSize = {8, 8};
+        PlayerCharacter playerCharacter = new PlayerCharacter(3, 0, boardSize);
+        int expectedPlayerX = 4;
+        int expectedPlayerY = 0;
+        playerCharacter.move(input);
+        int actualPlayerX = playerCharacter.getX();
+        int actualPlayerY = playerCharacter.getY();
 
+        assertEquals(expectedPlayerX, actualPlayerX);
+        assertEquals(expectedPlayerY, actualPlayerY);
+    }
+    @Test
+    void testMoveRightimpossible() {
+        char input = 'D';
+        int[] boardSize = {8, 8};
+        PlayerCharacter playerCharacter = new PlayerCharacter(7,0, boardSize);
+        int expectedPlayerX = 7;
+        int expectedPlayerY = 0;
+        playerCharacter.move(input);
+        int actualPlayerX = playerCharacter.getX();
+        int actualPlayerY = playerCharacter.getY();
+
+        assertEquals(expectedPlayerX, actualPlayerX);
+        assertEquals(expectedPlayerY, actualPlayerY);
     }
 }
